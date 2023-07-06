a8fa628201d3405baeb99971c2aca0b9138cd509
JJ-Feely <justin.feely@icloud.com>
2023-07-06 12:13:20 +0100

    new project

---
+++ SimpleApp/SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Michael Feely on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
